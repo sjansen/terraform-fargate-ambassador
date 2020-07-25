4a300aa5406c71e245ee1041079500d0ab755ba2
Stuart Jansen <sjansen@buscaluz.org>
2020-07-24 20:42:33 -0600

    reformat terraform files

--- config.tf
+++ config.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-2"
 }
 
 variable "ecr_prefix" {
-  type = string
+  type    = string
   default = "terraform-fargate-ambassador"
 }
 
 variable "queue_name" {
-  type = string
+  type    = string
   default = "terraform-fargate-ambassador"
 }
 
 variable "vpc_name" {
-  type = string
+  type    = string
   default = "terraform-fargate-ambassador"
 }
--- sqs.tf
+++ sqs.tf
@@ -9,12 +9,12 @@ data "aws_iam_policy_document" "queue" {
 }
 
 resource "aws_sqs_queue" "queue" {
-  name = var.queue_name
-  delay_seconds = 5
-  receive_wait_time_seconds = 20
+  name                       = var.queue_name
+  delay_seconds              = 5
+  receive_wait_time_seconds  = 20
   visibility_timeout_seconds = 600
 
-  kms_master_key_id = "alias/aws/sqs"
+  kms_master_key_id         = "alias/aws/sqs"
   message_retention_seconds = 1209600
-  policy = data.aws_iam_policy_document.queue.json
+  policy                    = data.aws_iam_policy_document.queue.json
 }
